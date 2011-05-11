612c25d92e4ac17adb9cdba08911a6ccb2869462
Markus Sitzmann <markus.sitzmann@gmail.com>
2011-05-10 23:57:52 -0400

    getting things ready for a first release; final bug fixes

--- examples.py
+++ examples.py
@@ -1,16 +1,22 @@
 from pychembl.settings import *
 from pychembl.db.auto_schema import *
 
-# loop over all assays containing the term "human" in their assay description
-assays = session.query(Assays).filter(Assays.description.like('%human%')).limit(1000).yield_per(100)
+# loop over the first 1000 assays containing the term "human" in their assay description; yield the
+# the result in blocks of 25 database rows
+
+assays = session.query(Assays).filter(Assays.description.like('%human%')).limit(1000).yield_per(25)
+
 for assay in assays:
-	print "- %s %s" % (assay.description, len(assay.targets))
+	print "- %s" % (assay.description)
 	
 print "\n\n\n"
 
-# select all 'Kallikrein 14' target entries (only one), find the related assays and print 
-# activities and canonical SMILES of the ligand
+
+# select all 'Kallikrein 14' target entries (yields only one), find the related assays and print 
+# activities and canonical SMILES of the ligand molecules
+
 targets = session.query(TargetDictionary).filter(TargetDictionary.pref_name=='Kallikrein 14').all()
+
 for target in targets:
 	print target.description
 	for assay in target.assays:
